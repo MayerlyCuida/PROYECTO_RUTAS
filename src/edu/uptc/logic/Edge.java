f2225c8808517040d20b243ca2ecac9481c5953e
mayerly.cuida@uptc.edu.co <mayerly.cuida@uptc.edu.co>
2024-07-01 13:03:58 -0500

    CLASE EDGE

---
+++ src/edu/uptc/logic/Edge.java
@@ -0,0 +1,36 @@
+package edu.uptc.logic;
+//Arista
+
+public class Edge {
+	private  int  	idEdge;
+	private  String nameEdge;
+	private int distance;
+	
+	public Edge(int idEdge, String nameEdge, int distance) {
+		super();
+		this.idEdge = idEdge;
+		this.nameEdge = nameEdge;
+		this.distance = distance;
+	}
+	public int getIdEdge() {
+		return idEdge;
+	}
+	public void setIdEdge(int idEdge) {
+		this.idEdge = idEdge;
+	}
+	public String getNameEdge() {
+		return nameEdge;
+	}
+	public void setNameEdge(String nameEdge) {
+		this.nameEdge = nameEdge;
+	}
+	public int getDistance() {
+		return distance;
+	}
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+	
+
+
+}
